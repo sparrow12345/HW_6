761ddf8f4a00f1cde9fc4dfba87b380a77547e86
sparrow12345 <m.naser@innopolis.university>
2023-04-04 13:57:58 +0300

    first commit

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,33 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8'),
+	() => import('./nodes/9')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [~2],
+		"/about": [3],
+		"/comic": [4],
+		"/education": [5],
+		"/experience": [6],
+		"/projects": [7],
+		"/sverdle": [~8],
+		"/sverdle/how-to-play": [9]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+};
+
+export { default as root } from '../root.svelte';
